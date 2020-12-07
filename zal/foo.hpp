0479ec41ee2a13eb32037cbd092f624732f2a0d0
BlazejMaciejski <56317717+BlazejMaciejski@users.noreply.github.com>
2020-12-07 12:08:23 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -1,12 +1,26 @@
-#pragma once
-
 #include "Human.hpp"
 
+#include <algorithm>
 #include <list>
 #include <vector>
 
 std::vector< char > foo(std::list< Human >& people)
 {
+    int                 i = 0;
+    std::vector< char > isAnimalLover;
+
+    for (Human person : people) {
+        person.birthday();
+
+        if (person.isMonster()) {
+            isAnimalLover[i] = 'y';
+        }
+        else {
+            isAnimalLover[i] = 'n';
+        }
+        i++;
+    }
+    std::reverse(isAnimalLover.begin(), isAnimalLover.end());
     // Twoja implementacja tutaj
     return {};
 }
